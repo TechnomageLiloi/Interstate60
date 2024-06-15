3a1eb6aa523850cd3357a8e16f9737f444aeedc4
Anton Moskalenko <moskalenko.anton@gmail.com>
2024-06-15 03:55:08 +0300

    [0.1.6] Reset I60 [take 2]

--- Install/Database.sql
+++
@@ -1,8 +0,0 @@
-create table rune_wiki
-(
-    key_wiki varchar(700) not null,
-    title varchar(300) not null,
-    article text not null,
-    constraint rune_wiki_pk
-        primary key (key_wiki)
-);
