eb19aa21ecffec104626bfa3973bfca9f6223601
Anton Moskalenko <moskalenko.anton@gmail.com>
2026-01-30 09:41:10 +0200

    [0.00.004] Add ticket domain.

---
+++ Engine/Domain/Tickets/Entity.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Liloi\I60\Domain\Tickets;
+
+use Liloi\Stylo\Parser;
+use Liloi\Tools\Entity as AbstractEntity;
+
+/**
+ * @method string getComment()
+ * @method void setComment(string $value)
+ */
+class Entity extends AbstractEntity
+{
+
+}
---
+++ Engine/Domain/Tickets/Manager.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Liloi\I60\Domain\Tickets;
+
+use Liloi\I60\Domain\Manager as DomainManager;
+
+class Manager extends DomainManager
+{
+
+}
