a55fc2c656bee58588f3b6619e92568b74a84801
Anton Moskalenko <moskalenko.anton@gmail.com>
2026-02-10 13:00:39 +0200

    [0.01.002] Add database SQL source.

--- Install/Database.sql
+++ Install/Database.sql
@@ -24,21 +24,78 @@ CREATE TABLE `i60_config` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `i60_tickets`
+-- Table structure for table `i60_logs`
 --
 
-DROP TABLE IF EXISTS `i60_tickets`;
+DROP TABLE IF EXISTS `i60_logs`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `i60_tickets` (
-  `key_ticket` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `start` timestamp NOT NULL,
-  `title` varchar(300) NOT NULL,
-  `status` tinyint unsigned NOT NULL DEFAULT '1',
-  `data` text NOT NULL,
-  PRIMARY KEY (`key_ticket`)
+CREATE TABLE `i60_logs` (
+  `key_log` bigint unsigned NOT NULL AUTO_INCREMENT,
+  `ts` timestamp NOT NULL,
+  `tags` varchar(1000) NOT NULL,
+  `data` json NOT NULL,
+  PRIMARY KEY (`key_log`)
 ) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `i60_epochs`
+--
+
+DROP TABLE IF EXISTS `i60_epochs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `i60_epochs` (
+  `key_epoch` tinyint unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `status` tinyint unsigned NOT NULL DEFAULT '1',
+  `summary` text NOT NULL,
+  `start` date NOT NULL,
+  `finish` date NOT NULL,
+  PRIMARY KEY (`key_epoch`)
+) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `i60_milestones`
+--
+
+DROP TABLE IF EXISTS `i60_milestones`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `i60_milestones` (
+  `key_milestone` tinyint unsigned NOT NULL,
+  `key_epoch` tinyint unsigned NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `status` tinyint unsigned NOT NULL DEFAULT '1',
+  `summary` text NOT NULL,
+  `start` date NOT NULL,
+  `finish` date NOT NULL,
+  PRIMARY KEY (`key_milestone`, `key_epoch`),
+  FOREIGN KEY (`key_epoch`) REFERENCES `i60_epochs` (`key_epoch`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+--
+-- Table structure for table `i60_quests`
+--
+
+CREATE TABLE `i60_quests` (
+  `key_quest` tinyint unsigned NOT NULL,
+  `key_milestone` tinyint unsigned NOT NULL,
+  `key_epoch` tinyint unsigned NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `status` tinyint unsigned NOT NULL DEFAULT '1',
+  `type` tinyint unsigned NOT NULL DEFAULT '1',
+  `summary` text NOT NULL,
+  `data` json NOT NULL,
+  PRIMARY KEY (`key_quest`, `key_milestone`, `key_epoch`),
+  FOREIGN KEY (`key_milestone`, `key_epoch`) REFERENCES `i60_milestones` (`key_milestone`, `key_epoch`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=cp1251;
+
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
